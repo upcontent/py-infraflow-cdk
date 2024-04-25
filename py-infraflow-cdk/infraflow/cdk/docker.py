b30d625f5f381eb94f2e2d8e6d0b27d7b7de7d63
Sabina Hartnett <sabina.hartnett@gmail.com>
2024-04-25 11:42:31 -0700

    follow init params from documentation

--- py-infraflow-cdk/infraflow/cdk/docker.py
+++ py-infraflow-cdk/infraflow/cdk/docker.py
@@ -106,7 +106,7 @@ class EcsCluster:
         ))
 
     def create_logs(self, service_name: str) -> (LogGroup, LogDriver):
-        log_group = LogGroup(service_name=service_name+"Logs")
+        log_group = LogGroup(id=service_name+"Logs")
         return log_group, LogDriver.aws_logs(stream_prefix="", log_group=log_group)
 
     def load_balanced_service(
